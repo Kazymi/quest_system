6870d8ca64268ac1087e2846192e245136b56d80
Evgeny <67922656+Kazymi@users.noreply.github.com>
2024-01-25 19:47:33 +0300

    Update ShrederUpgrade.cs

--- QuestSystem/Quest/ShrederUpgrade.cs
+++ QuestSystem/Quest/ShrederUpgrade.cs
@@ -3,26 +3,5 @@ using UnityEngine;
 
 public class ShrederUpgrade : MonoBehaviour
 {
-    private BuyPlace buyPlace;
-
-    private void Awake()
-    {
-        buyPlace = GetComponent<BuyPlace>();
-    }
-
-    private void OnEnable()
-    {
-        buyPlace.Bought += Bought;
-    }
-
-    private void OnDisable()
-    {
-        buyPlace.Bought -= Bought;
-    }
-
-    private void Bought()
-    {
-        Bootstrap.Instance.PlayerData.CurrentShrederBuildLevel++;
-        Bootstrap.Instance.SaveGame();
-    }
+    //todo delete old file
 }
